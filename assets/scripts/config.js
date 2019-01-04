9aeedf2de42fe678af728a056c5854711d8a46bf
jooewoo <jooewoo@gmail.com>
2019-01-04 11:54:07 -0500

    Added the heroku link to the config file

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://glacial-chamber-28137.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
