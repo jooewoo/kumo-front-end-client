2bc8c838f7fdc862eed53bef399b182d350d8c0c
Cid369 <waikit.tong.82@gmail.com>
2019-01-04 14:35:46 -0500

    * Add auth forms in html. * Add hidden in css * Got rid of old codes from startup codes.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,11 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+const authEvents = require('./auth/events.js')
+
 $(() => {
-  // your JS code goes here
+  $('#sign-up-form').on('submit', authEvents.onSignUp)
+  $('#sign-in-form').on('submit', authEvents.onSignIn)
+  $('#change-password-form').on('submit', authEvents.onChangePassword)
+  $('#sign-out-button').on('click', authEvents.onSignOut)
 })
